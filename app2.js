aa8f08f248c1e893876d5acc5cd38341cd48cb86
Luis Ramirez <lrxd2025@gmail.com>
2023-01-19 12:00:59 -0800

    added blog back and images to webp

--- app2.js
+++ app2.js
@@ -10,7 +10,7 @@ function renderPosts(doc) {
     div.setAttribute("class", "blog-info");
     let h2 = document.createElement("h2");
     let p = document.createElement("p");
-    let p1 = document.createElement("p");
+    let h3 = document.createElement("h3");
 
     //let img = document.createElement("IMG");
     //img.setAttribute("src");
@@ -21,10 +21,10 @@ function renderPosts(doc) {
     div.setAttribute('data-id', doc.id);
     h2.textContent = doc.data().postName;
     p.textContent = doc.data().postContent;
-    p1.textContent = doc.data().createdAt;
+    h3.textContent = doc.data().createdAt;
 
     div.appendChild(h2);
-    div.appendChild(p1);
+    div.appendChild(h3);
     div.appendChild(p);
 
     postList.appendChild(div);
@@ -41,16 +41,16 @@ db.collection('posts').orderBy('createdAt', 'desc', "postName").get().then((snap
 //saving data 
 //not using rn because of dont need clients to post only me rn
 
-//form.addEventListener('submit', (e) => {
-//  e.preventDefault();
-//db.collection('posts').add({
-//  author: form.author.value,
-//postName: form.postTitle.value,
-// postContent: form.postContent.value,
-// createdAt: form.postDate.value
-// });
-//  form.author.value = '';
-//  form.postTitle.value = '';
-//  form.postContent.value = '';
-//  form.postDate.value = '';
-//});
+form.addEventListener('submit', (e) => {
+ e.preventDefault();
+db.collection('posts').add({
+ author: form.author.value,
+postName: form.postTitle.value,
+postContent: form.postContent.value,
+createdAt: form.postDate.value
+});
+ form.author.value = '';
+ form.postTitle.value = '';
+ form.postContent.value = '';
+ form.postDate.value = '';
+});
